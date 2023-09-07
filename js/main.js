82b01b35aba75f8bbc66a98ac841e4fcf0cb706f
isupov <t.1113@ya.ru>
2023-09-07 15:01:12 +0300

    Added additional whitespaces

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ document.addEventListener( 'DOMContentLoaded', function () {
   const container = document.querySelector( '.button-marker-container' );
 
   function deactivateActiveButtons() {
-    const allActiveButtons = document.querySelectorAll('.button-marker[data-active]');
-    allActiveButtons.forEach(button => {
-      button.removeAttribute('data-active');
-    });
+    const allActiveButtons = document.querySelectorAll( '.button-marker[data-active]' );
+    allActiveButtons.forEach( button => {
+      button.removeAttribute( 'data-active' );
+    } );
   }
 
   container.addEventListener( 'click', function ( event ) {
@@ -20,11 +20,11 @@ document.addEventListener( 'DOMContentLoaded', function () {
       return;
     }
 
-    if (target.hasAttribute('data-active')) {
+    if ( target.hasAttribute( 'data-active' ) ) {
       deactivateActiveButtons();
     } else {
       deactivateActiveButtons();
-      target.setAttribute('data-active', 'true');
+      target.setAttribute( 'data-active', 'true' );
     }
   } );
 } );
