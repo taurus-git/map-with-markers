56e88cc1250226e50a8c206230042ea867207e1e
isupov <t.1113@ya.ru>
2023-09-07 11:47:35 +0300

    Added markup for the map-with-markers, button-marker, a cross sign inside. Added styles for those elements, base styles for the body. Added js file to catch click on a button add / remove data attributes. Added  css variables

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const container = document.querySelector('.map-with-markers');
+  let activeButton = null;
 
+  container.addEventListener('click', function(event) {
+    if (activeButton) {
+      activeButton.removeAttribute('data-active');
+    }
+
+    let target = event.target;
+
+    if (target.classList.contains('cross')) {
+      target = target.parentNode;
+    }
+
+    if (target.classList.contains('button-marker')) {
+      if (activeButton === event.target) {
+        activeButton = null;
+        return;
+      }
+
+      activeButton = event.target;
+      activeButton.setAttribute('data-active', 'true');
+    } else {
+      activeButton = null;
+    }
+  });
+});
